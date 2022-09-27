8fa7170fb4b609950fdcc03477e7d2359892eca8
elozano2022 <dnart01@gmail.com>
2022-09-27 17:54:26 -0500

    configuracion completa de Django

--- virtual/authProject/settings.py
+++ virtual/authProject/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'Panda_Art',
     
 
 ]
@@ -79,7 +80,7 @@ WSGI_APPLICATION = 'authProject.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3', 
     }
 }
 
